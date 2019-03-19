a897530491753a348d8249fc9aee00ba4f4cdc60
ingridLarissa <48722564+ingridLarissa@users.noreply.github.com>
2019-03-19 11:52:54 -0300

    Add files via upload

--- controleNota.php
+++ controleNota.php
@@ -18,6 +18,17 @@
 			echo "<br>n2 = " . $n2;
 			echo "<br>n3  = " . $n3;
 			echo "<br>n4 = " . $n4;
+						
+			$media = ($n1 + $n2 + $n3 + $n4)/4;
+			if($media>6) {			
+				echo "<p class=aprovado>Parabéns " . $aluno . ", você foi aprovado com média  = " . $media . "</p>";
+				}
+				
+			else {
+				echo"<p class=reprovado> Puxa vida" . $aluno . ", você foi reprovado com mádia =" . $media ."</p>";
+			
+			}
+			
 			echo"</fieldset>";
 	}else {
 		echo "Campos não preenchidos!!!";
